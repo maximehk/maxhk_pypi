f2ab1d865e50d266a06572efbc17ec6e2d7312aa
Max <max@noreply.com>
2022-11-05 22:42:32 +0100

    Updated the 1password package to handle sections.

--- src/maxhk/op.py
+++ src/maxhk/op.py
@@ -10,9 +10,35 @@ def __check_op() -> None:
         raise RuntimeError("`op` is not installed")
 
 
-def get_item(item_id: str, fields: List[str]) -> namedtuple:
+def _build_id(entry):
+    section = entry.get("section", {}).get("label")
+    if section:
+        return "_".join([section, entry.get("label")])
+    return entry.get("label")
+
+
+def _get_item(item_id: str) -> dict:
     __check_op()
-    ResultType = namedtuple("Item", fields)
-    csv_fields = ",".join(fields)
-    data = sp.getoutput(f'op item get {item_id} --format json --fields "{csv_fields}"')
-    return ResultType(*[x["value"] for x in json.loads(data)])
+    raw_data = sp.getoutput(f"op item get {item_id} --format json")
+    data = json.loads(raw_data).get("fields", [])
+    return {_build_id(x): x.get("value") for x in data}
+
+
+def get_item(item_id: str, fields: List[str] = None) -> namedtuple:
+    """Retrieves an item from 1password.
+
+    Keyword arguments:
+    item_id -- first parameter to `op item get`.
+    fields  -- fields to include in the result tuple. All fields are included
+    when this argument is None or empty.
+    """
+    __check_op()
+    fields = fields or []
+    data = _get_item(item_id)
+    all_fields = set(data)
+    missing_fields = set(fields) - all_fields
+    if missing_fields:
+        raise KeyError("Missing fields: %s" % ", ".join(missing_fields))
+    fields_to_return = fields or all_fields
+    ResultType = namedtuple("Item", fields or fields_to_return)
+    return ResultType(**{k: v for (k, v) in data.items() if k in fields_to_return})
