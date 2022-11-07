3147d58cae05e535f29c99265487fcff28093baf
Max <max@noreply.com>
2022-11-07 10:50:13 +0100

    migrated to poetry

--- update_pypi.py
+++ update_pypi.py
@@ -1,19 +1,6 @@
-#!/usr/bin/env python3
+#!/bin/bash
 
-from src.maxhk import op  # a bit hacky but allows running `op` prior to install
-import subprocess as sp
-import shutil
-import os
+username="$(op item get 'pypi.org' --format json --fields 'username' | jq -r '.value')"
+password="$(op item get 'pypi.org' --format json --fields 'password' | jq -r '.value')"
+python3 -m poetry publish --build -u "${username}" -p "${password}"
 
-def main():
-    pypi_creds = op.get_item("pypi.org", ["username", "password"])
-    shutil.rmtree('dist', ignore_errors=True)
-    assert sp.check_call("python3 -m build", shell=True) == 0
-    sp.Popen(
-        f'python3 -m twine  upload -u "{pypi_creds.username}" -p "{pypi_creds.password}"  dist/*',
-        shell=True,
-    )
-
-
-if __name__ == "__main__":
-    main()
